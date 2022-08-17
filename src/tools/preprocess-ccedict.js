7248730ceab80290e41c3cadbf156863da13100a
Ben Stein <ben.s.stein@gmail.com>
2022-08-17 07:41:15 -0400

    Rollback to addition of cedict, we're going to preprocess it instead

---
+++ src/tools/preprocess-ccedict.js
@@ -0,0 +1,5 @@
+/*
+    CC-Cedict comes in a long line-based format, and its pinyin is also not ideally
+    formatted (e.g. huo3 shan1). This thing acts on "./data/cedict_ts.u8" (git excluded) and outputs
+    JSON with ideally formatted pinyin.
+*/
